f022de99bb8a3e96dcbac5daf954532242094684
rjan90 <8628857+rjan90@users.noreply.github.com>
2026-03-31 08:22:07 +0000

    Generated Homebrew formulas for the latest release (node=v1.35.1, miner=miner/v1.35.1).

--- Formula/lotus-miner.rb
+++ Formula/lotus-miner.rb
@@ -2,15 +2,15 @@
 class LotusMiner < Formula
   desc "A homebrew cask for installing the Lotus miner from filecoin-project/lotus"
   homepage "https://filecoin.io"
-  version "1.35.0"
+  version "1.35.1"
   license "MIT"
 
   depends_on "hwloc"
 
   on_macos do
     on_arm do
-      url "https://github.com/filecoin-project/lotus/releases/download/miner/v1.35.0/lotus-miner_v1.35.0_darwin_arm64.tar.gz"
-      sha256 "45601673abf63dbfd4c34562c1a770e1eab71db2e1c8707ac4853fa45b05a2e2"
+      url "https://github.com/filecoin-project/lotus/releases/download/miner/v1.35.1/lotus-miner_v1.35.1_darwin_arm64.tar.gz"
+      sha256 "86e7698267e54de6fead2f1c25cfb12b1f3103c838f499f83d7bcb5cedc56a1a"
 
       def install
         bin.install "lotus-miner"
@@ -22,8 +22,8 @@ class LotusMiner < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/filecoin-project/lotus/releases/download/miner/v1.35.0/lotus-miner_v1.35.0_linux_amd64_v1.tar.gz"
-        sha256 "48b4594b8449a1e9b5ce6ac166f70f82b2f8d0f823d3cbe148a177b8832cf4f6"
+        url "https://github.com/filecoin-project/lotus/releases/download/miner/v1.35.1/lotus-miner_v1.35.1_linux_amd64_v1.tar.gz"
+        sha256 "e834744aa33210fff56142253e1e8ef33efbcd15f18b983c37c069ea642f1d0c"
 
         def install
           bin.install "lotus-miner"
--- Formula/lotus.rb
+++ Formula/lotus.rb
@@ -2,15 +2,15 @@
 class Lotus < Formula
   desc "A homebrew cask for installing the Lotus node from filecoin-project/lotus"
   homepage "https://filecoin.io"
-  version "1.35.0"
+  version "1.35.1"
   license "MIT"
 
   depends_on "hwloc"
 
   on_macos do
     on_arm do
-      url "https://github.com/filecoin-project/lotus/releases/download/v1.35.0/lotus_v1.35.0_darwin_arm64.tar.gz"
-      sha256 "4292027ec10c2955266f7fe8a11e217f770c60101e47e28069c19b59e5b3c1f7"
+      url "https://github.com/filecoin-project/lotus/releases/download/v1.35.1/lotus_v1.35.1_darwin_arm64.tar.gz"
+      sha256 "1a086a36463f735c05f173b1aa036bc1c926fb28a7124bda8922d4d787120aaf"
 
       def install
         bin.install "lotus"
@@ -21,8 +21,8 @@ class Lotus < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/filecoin-project/lotus/releases/download/v1.35.0/lotus_v1.35.0_linux_amd64_v1.tar.gz"
-        sha256 "4eda5a7a4952c9523a2b168829606874f02209631d60a59f751a90868e655192"
+        url "https://github.com/filecoin-project/lotus/releases/download/v1.35.1/lotus_v1.35.1_linux_amd64_v1.tar.gz"
+        sha256 "53bf47ba63b4ddc6afe12e9eda28e9dfd76a7e4a3174f3bd44523110f7f61802"
 
         def install
           bin.install "lotus"
